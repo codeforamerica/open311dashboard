5c7cf3109c5b524099179332e26f67fbc91ee520
Alan Palazzolo <github@zzolo.org>
2011-03-25 16:02:27 -0700

    Fixed some commit errors with html and JS.

--- tests/js/widget-tester.js
+++ tests/js/widget-tester.js
@@ -3,10 +3,10 @@
  */
 (function( $, undefined ) {
 
-$('.widget.bargraph-type').bargraphType({dataSource: 'data/open_requests_by_type.json'});
-$('.widget.sparkline').sparkline();
-$('.widget.sparkline').pieType();
-$('.widget.map').map();
+$('.bargraph-type').bargraphType({dataSource: 'data/open_requests_by_type.json'});
+$('.sparkline').sparkline();
+$('.pie-type').pieType();
+$('.map').map();
 $('.top-open-requests').topOpenRequests();
 
 })( jQuery );
