0e90bd1902575de36cf63dc022a6bed1c0cace3d
Mjumbe Wawatu Poe <mjumbe@codeforamerica.org>
2011-03-25 14:01:16 -0700

    Be able to make space for a 1x1 item in the grid.  And generate the structure of the grid dynamically.

--- dashboard_container/dashboard_container.js
+++ dashboard_container/dashboard_container.js
@@ -12,20 +12,34 @@
 	GridColumn = function(jqo, index) {
 		this.jqobject = jqo
 		this.colindex = index
+		this.cells = []
 		
 		jqo.addClass("grid-column");
 	}
 	
-	function prepare_grid(jqobject) {
+	GridCell = function(jqo, index) {
+		this.jqobject = jqo
+		
+		jqo.addClass("grid-cell");
+	}
+	
+	function prepare_grid(jqobject, num_cols) {
 		var items = []
 		var columns = []
+		
+		// alter the dom
+		jqobject.children().wrapAll('<div class="grid-items"></div>')
+		jqobject.append('<div class="grid-columns"></div>')
+		for (var n = 0; n < num_cols; n++) {
+			jqobject.find('.grid-columns').append('<ul></ul>')
+		}
 		
 		var column_nodes = jqobject.find(".grid-columns > ul")
 		var item_nodes = jqobject.find(".grid-items > *")
 		
 		column_nodes.each(function(index, column_node) {
 			column = new GridColumn($(column_node))
-			columns.push(column, index)
+			columns.push(column)
 		});
 		
 		item_nodes.each(function(index, item_node) {
@@ -46,17 +60,21 @@
 		 * be placed within the columns.  For now will just use the first 
 		 * available space (and assume that all items will be 1x1). */
 		
-		var cell_to_place = null;
+		var column_to_place_in = null;
 		for (var c in columns) {
-			column = columns[c];
-			var current_cell = column.get_lowest_cell()
-			if (!cell_to_place) {
-				cell_to_place = current_cell
+			var current_column = columns[c];
+			if (column_to_place_in === null) {
+				column_to_place_in = current_column
 			}
-			else if (current_cell && 
-			    current_cell.is_higher_than(cell_to_place)) {
-				cell_to_place = current_cell
+			else if (
+				current_column.cells.length < column_to_place_in.cells.length) {
+				column_to_place_in = current_column
 			}
 		}
+		
+		var cell = new GridCell($('<li></li>'))
+		column_to_place_in.cells.push(cell)
+		column_to_place_in.jqobject.append(cell.jqobject)
+		cell.jqobject.addClass('empty-cell')
 	}
 
