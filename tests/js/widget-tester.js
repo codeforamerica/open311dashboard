d6ac27112beb48158bffb5af5ca846eda2584b34
Tyler Stalder <tyler@tylerstalder.com>
2011-03-25 15:56:06 -0700

    add map widget

--- tests/js/widget-tester.js
+++ tests/js/widget-tester.js
@@ -6,5 +6,6 @@
 $('.widget.bargraph-type').bargraphType({dataSource: 'data/open_requests_by_type.json'});
 $('.widget.sparkline').sparkline();
 $('.widget.sparkline').pieType();
+$('.widget.map').map();
 
 })( jQuery );
