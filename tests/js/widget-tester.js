0ee46ba9fb2f6e091ff1093834e9fdb0f60ed71b
Alan Palazzolo <github@zzolo.org>
2011-03-25 20:00:50 -0700

    Removed redundant, not as cool widgets.

--- tests/js/widget-tester.js
+++ tests/js/widget-tester.js
@@ -8,9 +8,7 @@ $('.map-google-example').mapGoogleExample({dataSource: 'data/ServiceRequests_Com
 
 $('.gauge').gauge();
 $('#search').searchType();
-$('.bargraph-type').bargraphType({dataSource: 'data/open_requests_by_type.json'});
 $('.sparkline').sparkline();
-$('.pie-type').pieType();
 // $('.map').map();
 $('.open-vs-closed').openVsClosed();
 
