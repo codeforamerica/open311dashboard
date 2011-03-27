cacd7a5a5bce5a2da865850c78689d503eb0cc10
Michael Lawrence Evans <michael@longliveman.com>
2011-03-26 23:47:46 -0700

    formatting test page

--- tests/js/widget-tester.js
+++ tests/js/widget-tester.js
@@ -9,7 +9,7 @@ $('.sparkline').sparklineExample();
 $('.open-vs-closed').pieRaphaelOpenClosed();
 $('#search').searchType();
 
-$('.gauge').gaugeGoogleActualEstResponseTime({dataSource: 'data/ServiceRequests_Complete.json'});
+//$('.gauge').gaugeGoogleActualEstResponseTime({dataSource: 'data/ServiceRequests_Complete.json'});
 // $('.map').map();
 
 
