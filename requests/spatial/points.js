ac0d053710ad66763a166f9f8e1edd56366c24bf
Tyler Stalder <tyler@stalder.me>
2011-03-27 22:11:33 -0700

    removed redundant requests dir, this is now in the couchapp folder and up to date

--- requests/spatial/points.js
+++
@@ -1,8 +0,0 @@
-function(doc) {
-	if (doc.status == "Open") {
-    	emit({
-        	type: "Point",
-        	coordinates: [doc.lon, doc.lat]
-    		}, 1);
-	}
-}
--- requests/views/openbytime/map.js
+++
@@ -1,5 +0,0 @@
-function(doc) { 
-	if(doc.status == 'Open') { 
-		emit(doc.requested_datetime,doc);
-	}
-}
