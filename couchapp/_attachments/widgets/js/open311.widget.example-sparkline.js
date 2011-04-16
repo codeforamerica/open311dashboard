38144121b77df40e86a162ee89dc057f1531cee5
Michael Lawrence Evans <michael@codeforamerica.org>
2011-04-16 10:43:04 -0700

    fixing the sorting of data in the sparkline example

--- couchapp/_attachments/widgets/js/open311.widget.example-sparkline.js
+++ couchapp/_attachments/widgets/js/open311.widget.example-sparkline.js
@@ -45,17 +45,20 @@ $.widget('Open311.sparklineExample', $.Open311.sparklineGoogle, {
 
 	    //Adding our live data from March and April; it is sorted on the server.
 	    for(var i = 0; i < data.rows.length; i++){
-		new_monthly_values.push({count: data.rows[i].value, position: month_map[data.rows[i].key]});
+		//new_monthly_values.push({count: data.rows[i].value, position: month_map["January"]});
+		new_monthly_values.push({count: data.rows[i].value, position: month_map[data.rows[i].key.substring(5,data.rows[i].key.length)]});
 	    }
-	    
+	    console.log(new_monthly_values[0].value)
 	    new_monthly_values.sort(function(a,b){return a.position - b.position}); //.count?
 	    for(i in new_monthly_values){console.log(new_monthly_values[i].count)};
 	    var new_monthly_values_sorted = [];
 	    for(i in new_monthly_values){new_monthly_values_sorted[i] = new_monthly_values[i].count};
+	    //console.log(new_monthly_values_sorted)
 	    var monthly_values = [];
 	    monthly_values = monthly_values.concat(old_monthly_values,new_monthly_values_sorted);
+	    var last_month_count = new_monthly_values_sorted[new_monthly_values_sorted.length-1];
 
-	    self.updateContent('<img src="http://chart.apis.google.com/chart?cht=lc&chs=400x70&chds=0,2438&chd=t:' + monthly_values.join(',') + '&chco=336699&chls=1,1,0&chm=o,990000,0,13,4&chxt=r,x,y&chxs=0,990000,11,0,_|1,990000,1,0,_|2,990000,1,0,_&chxl=0:|1173|1:||2:||&chxp=0,45.80"></img>');
+	    self.updateContent('<img src="http://chart.apis.google.com/chart?cht=lc&chs=400x70&chds=0,2438&chd=t:' + monthly_values.join(',') + '&chco=336699&chls=1,1,0&chm=o,990000,0,13,4&chxt=r,x,y&chxs=0,990000,11,0,_|1,990000,1,0,_|2,990000,1,0,_&chxl=0:|' + last_month_count + '|1:||2:||&chxp=0,45.80"></img>');
 
 	} else {
 	    self.valueDiv = $('<div class="no-data">No data.</div>')
