fd5b13f2fcdc143004f74d9748a540d1bf3cdcaa
Michael Lawrence Evans <michael@codeforamerica.org>
2011-06-05 16:37:06 -0700

    add ability to switch between map types

--- src/js/streets_ray_casting1.js
+++ src/js/streets_ray_casting1.js
@@ -67,7 +67,8 @@ function onloadneighborhoods(e){
 }
 
 function updateNeighborhoodStats(feature) {
-  $neighborhoodStats.html(feature.data.properties.neighborhood);
+//'<strong>'+score+'</strong></br><span class="dynamicsparkline1">Loading..</span>'
+  $neighborhoodStats.html('<span>'+feature.data.properties.neighborhood+'</span><table><tr><th>Total</th><th>Rank</th></tr><tr><td>'+feature.data.properties.total+'</td><td>'+feature.data.properties.rank+'</td></tr></table><br><table><tr><th>Request</th><th>Total</th></tr><tr><td>'+feature.data.properties.top_five[4][0]+'</td><td>'+feature.data.properties.top_five[4][1]+'</td></tr><tr><td>'+feature.data.properties.top_five[3][0]+'</td><td>'+feature.data.properties.top_five[3][1]+'</td></tr><tr><td>'+feature.data.properties.top_five[2][0]+'</td><td>'+feature.data.properties.top_five[2][1]+'</td></tr><tr><td>'+feature.data.properties.top_five[1][0]+'</td><td>'+feature.data.properties.top_five[1][1]+'</td></tr><tr><td>'+feature.data.properties.top_five[0][0]+'</td><td>'+feature.data.properties.top_five[0][1]+'</td></tr></table>');
 }
 
 function testNeighborhood(e){
@@ -124,20 +125,44 @@ function follow(e){
 
 function setsparkline(score,months){
       var monthly_values = [months[11],months[0],months[1],months[2],months[3],months[4]];
-      $('.dynamicsparkline1').sparkline(monthly_values,{width:100}); //why does this work here?
+      $('.dynamicsparkline1').sparkline(monthly_values,{width:100,fillColor:false,lineColor:"#336699",minSpotColor:"#990000",maxSpotColor:"#990000",spotColor:"#990000",lineWidth:1}); //why does this work here?
+}
+
+function setbarchart(score){
+       //var monthly_values = [months[11],months[0],months[1],months[2],months[3],months[4]];
+       //range is an array: range[0],range[1],range[2],range[3],range[4],range[5]
+
+        range = [100,200,300,200,100,20];
+        
+        if (score < 96){
+            cMap = ["red","green","green","green","green","green"];
+        } else if (score < 192) {
+            cMap = ["green","red","green","green","green","green"];
+        } else if (score < 288) {
+            cMap = ["green","green","red","green","green","green"];
+        } else if (score < 384) {
+            cMap = ["green","green","green","red","green","green"];
+        } else if (score < 480){
+            cMap = ["green","green","green","green","red","green"];
+        } else {
+            cMap = ["green","green","green","green","green","red"];
+        };
+       
+      $('.dynamicbarchart').sparkline(range,{type:'bar',width:100,colorMap:cMap}); //why does this work here?
 }
 
 function onload(e){
-    var scores = [];
+    //var scores = [];
+    alert('onload');
 
     var colorArray = ['#D92B04','#A61103'];
     
     for(var i = 0; i < e.features.length; i++) {
-        var streetMouseOver = function(score,months, index){
+        var streetMouseOver = function(score,start_street,end_street,street,months,top_request,index){
             return function(evt){
-              setStreetContent(evt,score,months,index);  
+              setStreetContent(evt,score,start_street,end_street,street,months,top_request,index);  
             };
-        }(e.features[i].data.properties.score,e.features[i].data.properties.months,i);
+        }(e.features[i].data.properties.score,e.features[i].data.properties.RT_FADD,e.features[i].data.properties.RT_TOADD,e.features[i].data.properties.STREETN_GC,e.features[i].data.properties.months,e.features[i].data.properties.top_request_type,i);
 
         e.features[i].element.onmouseover = streetMouseOver;
         e.features[i].element.onmousemove = follow;
@@ -157,23 +182,64 @@ function onload(e){
     }
 }
 
-function setStreetContent(e,score,months,index){
+function setStreetContent(e,score,start_street,end_street,street,months,top_request,index){
     //console.log('e',e);
 
     testNeighborhood(e);
 
-    var myvalues = [10,8,5,7,4,4,1];
-    $('.dynamicsparkline1').sparkline(myvalues);
-
-    $('#tooltip').html('<strong>'+score+'</strong></br><span class="dynamicsparkline1">Loading..</span>');
+    $('#tooltip').html('<span>'+parseInt(start_street,10) +' - ' + parseInt(end_street,10) + ' ' + street+'</span><br><strong>'+score+'</strong></br>Total Requests <span class="dynamicsparkline1">Loading..</span> '+months[4] + ' in May<br><span>Top Request Type: '+top_request[1]+'</span>');
     $('#tooltip').show();
     setsparkline(score,months);
 }
 
 function hideStreetContent(e){
-    //this is not getting called
     testNeighborhood(e);
 
+    $('#tooltip').hide();
+}
+
+function onresponseload(e){
+
+    alert('onresponseload');
+    var colorArray = ['#000000','#FF0000'];
+    
+    for(var i = 0; i < e.features.length; i++) {
+        var streetMouseOver = function(score,start_street,end_street,street,index){
+            return function(evt){
+              setResponseContent(evt,score,start_street,end_street,street,index);  
+            };
+        }(e.features[i].data.properties.response_time,e.features[i].data.properties.RT_FADD,e.features[i].data.properties.RT_TOADD,e.features[i].data.properties.STREETN_GC,i);
+
+        e.features[i].element.onmouseover = streetMouseOver;
+        e.features[i].element.onmousemove = follow;
+        e.features[i].element.onmouseout = hideResponseContent;
+        
+
+        if (e.features[i].data.properties.response_time < 480){
+            e.features[i].element.setAttribute("stroke",colorArray[0]);
+            e.features[i].element.setAttribute("stroke-opacity", 0.75);
+        } else {
+            e.features[i].element.setAttribute("stroke",colorArray[1]);
+            e.features[i].element.setAttribute("stroke-opacity", 0.8);
+        }
+
+
+        e.features[i].element.setAttribute("stroke-linecap","round");
+    }
+}
+
+function setResponseContent(e,score,start_street,end_street,street){
+    testNeighborhood(e);
+
+    $('#tooltip').html('<span>'+parseInt(start_street,10) +' - ' + parseInt(end_street,10) + ' ' + street+'</span><br><strong>'+score+'</strong></br>Total Requests <span class="dynamicbarchart">Loading..</span>');
+
+    $('#tooltip').show();
+
+    setbarchart(score);
+}
+
+function hideResponseContent(e){
+    testNeighborhood(e);
     $('#tooltip').hide();
 }
 
@@ -187,7 +253,7 @@ var map = po.map()
     .container(document.getElementById("map").appendChild(po.svg("svg")))
     .center({lat: 37.76, lon: -122.44})
     .zoom(13)
-    .zoomRange([12, 17])
+    .zoomRange([12, 16])
     .add(po.interact());
 
 map.add(po.image()
@@ -196,18 +262,22 @@ map.add(po.image()
     + "/38747/256/{Z}/{X}/{Y}.png")
     .hosts(["a.", "b.", "c.", ""])));
 
-//var density_map = po.image().url(po.url("http://ec2-184-73-13-139.compute-1.amazonaws.com:8888/1.0.0/open311_test/{Z}/{X}/{T}.png"));
+var response_time_map = po.image().url(po.url("http://ec2-184-73-13-139.compute-1.amazonaws.com:8888/1.0.0/open311_test2_548404/{Z}/{X}/{T}.png"));
+map.add(response_time_map);
+response_time_map.visible(false);
+
 var density_map = po.image().url(po.url("http://ec2-184-73-13-139.compute-1.amazonaws.com:8888/1.0.0/Open311_Density_Final/{Z}/{X}/{T}.png"));
 map.add(density_map);
 
-//http://ec2-184-73-13-139.compute-1.amazonaws.com:8888/1.0.0/open311_test/{Z}/{X}/{T}.png
-
-//http://ec2-184-73-13-139.compute-1.amazonaws.com:8888/1.0.0/open311_test2_548404/{z}/{x}/{y}
-
-//map.add(po.image().url(po.url("http://ec2-184-73-13-139.compute-1.amazonaws.com:8888/1.0.0/open311_test2_548404/{Z}/{X}/{T}.png")));
-
-map.add(po.geoJson()
-    .url("data/sf_polygons_geojson.json")
+/*
+map.add(po.image()
+    .url(po.url("http://{S}tile.cloudmade.com"
+    + "/1a193057ca6040fca68c4ae162bec2da"
+    + "/38933/256/{Z}/{X}/{Y}.png")
+    .hosts(["a.", "b.", "c.", ""])));
+*/
+var sf_neighborhoods = map.add(po.geoJson()
+    .url("data/sf_polygons_geojson_final.json")
     .id("neighborhoods")
     .zoom(12)
     .tile(false)
@@ -217,6 +287,17 @@ map.add(po.geoJson()
     //.title(function(d) { return d.properties.STREET + ": " + d.properties.PCI + " PCI"; })
     ));
 
+var density_lines = po.geoJson()
+    .url("data/scored_centerlines_sub_final.json")
+    .id("streets")
+    .zoom(12)
+    .tile(false)
+    .on("load", onload
+    //.on("load", po.stylist()
+    //.attr("stroke", function(d) { return color(d.properties.score).color; })
+    //.title(function(d) { return d.properties.STREET + ": " + d.properties.PCI + " PCI"; })
+    );
+/*
 map.add(po.geoJson()
     .url("data/scored_centerlines_sub_final.json")
     .id("streets")
@@ -227,6 +308,33 @@ map.add(po.geoJson()
     //.attr("stroke", function(d) { return color(d.properties.score).color; })
     //.title(function(d) { return d.properties.STREET + ": " + d.properties.PCI + " PCI"; })
     ));
+*/
+map.add(density_lines);
+var response_lines = po.geoJson()
+    .url("data/avg_response_times_sub.json")
+    .id("responses")
+    .zoom(12)
+    .tile(false)
+    .on("load", onresponseload
+    //.on("load", po.stylist()
+    //.attr("stroke", function(d) { return color(d.properties.score).color; })
+    //.title(function(d) { return d.properties.STREET + ": " + d.properties.PCI + " PCI"; })
+    );
+
+/*
+map.add(po.geoJson()
+    .url("data/average_response_times_sub.json")
+    .id("streets")
+    .zoom(12)
+    .tile(false)
+    .on("load", onresponseload
+    //.on("load", po.stylist()
+    //.attr("stroke", function(d) { return color(d.properties.score).color; })
+    //.title(function(d) { return d.properties.STREET + ": " + d.properties.PCI + " PCI"; })
+    ));
+*/
+map.add(response_lines);
+//response_lines.visible(false);
 
 map.add(po.compass()
     .pan("none"));
