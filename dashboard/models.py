90e8bb11f98205bbc6a673287a52b1b6a4dfdd1b
Chris Barna <chris@unbrain.net>
2011-06-15 15:11:27 -0700

    Created 'Request' model.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Request(models.Model):
+    service_request_id = models.IntegerField()
+    status = models.CharField(max_length=10)
+    status_notes = models.TextField()
+    service_name = models.CharField(max_length=100)
+    service_code = models.IntegerField()
+    description = models.TextField()
+    agency_responsible = models.CharField(max_length=255)
+    service_notice = models.CharField(max_length=255)
+    requested_datetime = models.DateTimeField()
+    updated_datetime = models.DateTimeField(null=True, blank=True)
+    expected_datetime = models.DateTimeField(null=True, blank=True)
+    address = models.CharField(max_length=255)
+    address_id = models.IntegerField()
+    zipcode = models.IntegerField()
+    lat = models.FloatField()
+    long = models.FloatField()
