fe6aea761f9cc671d042cccf4a2758e003ed5d9d
Chris Barna <chris@unbrain.net>
2011-06-16 11:32:51 -0700

    Set relative TEMPLATE_DIRS in settings.py.

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
+# Set PATH
+import os
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
 # Django settings for open311dashboard project.
 
 DEBUG = True
@@ -103,6 +107,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'open311dashboard.urls'
 
 TEMPLATE_DIRS = (
+        os.path.join(SITE_ROOT, 'dashboard/templates')
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
