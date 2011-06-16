1fa14d3aa19c576c799b705b2a446dac215426e9
Chris Barna <chris@unbrain.net>
2011-06-16 15:08:08 -0700

    Created extremely primative index view.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from dashboard.models import Request
+
+from django.template import Context
+from django.shortcuts import render
+
+def index(request):
+    request_list = Request.objects.all()[:10]
+    c = Context({
+        'request_list': request_list
+        })
+    return render(request, 'index.html', c)
--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'dashboard.views.index')
     # Examples:
     # url(r'^$', 'open311dashboard.views.home', name='home'),
     # url(r'^open311dashboard/', include('open311dashboard.foo.urls')),
