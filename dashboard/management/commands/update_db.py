e9abfcb1d4eeb388dae84fa63f3673b6b9fe2c10
Chris Barna <chris@unbrain.net>
2011-06-16 09:55:06 -0700

    Added basic error handling to the model inserts.

--- dashboard/management/commands/update_db.py
+++ dashboard/management/commands/update_db.py
@@ -12,12 +12,12 @@ ONE_DAY = dt.timedelta(days=1)
 def get_time_range(on_day=None):
     if on_day is None:
         on_day = dt.datetime.utcnow() - ONE_DAY
-    
+
     # End at the begining of on_day; start at the beginning of the previous
     # day.
     end = on_day.replace(hour=0, minute=0, second=0, microsecond=0)
     start = end - ONE_DAY
-    
+
     return (start, end)
 
 def validate_dt_value(datetime):
@@ -28,7 +28,7 @@ def validate_dt_value(datetime):
     """
     if datetime.microsecond != 0:
         raise ValueError('Microseconds on datetime must be 0: %s' % datetime)
-    
+
     if datetime.tzinfo is not None:
         raise ValueError('Tzinfo on datetime must be None: %s' % datetime)
 
@@ -36,13 +36,13 @@ def get_requests_from_SF(start,end):
     """
     Retrieve the requests from the San Francisco 311 API within the time range
     specified by the dates start and end.
-    
+
     Returns a stream containing the content from the API call.
     """
-    
+
     validate_dt_value(start)
     validate_dt_value(end)
-    
+
     #url = r'https://open311.sfgov.org/dev/Open311/v2/requests.xml' #dev
     url = r'https://open311.sfgov.org/Open311/v2/requests.xml'
     query_data = {
@@ -51,7 +51,7 @@ def get_requests_from_SF(start,end):
         'jurisdiction_id' : 'sfgov.org'
     }
     query_str = urllib.urlencode(query_data)
-    
+
     requests_stream = urllib2.urlopen(url + '?' + query_str)
     return requests_stream
 
@@ -62,33 +62,33 @@ def parse_requests_doc(stream):
     """
 
     import xml.dom
-    
+
     xml_string = stream.read()
 
     columns = [] #holding columns for a day's worth of incident data
     values = [] #holding values for a day's worth of incident data
-    
+
     try:
         requests_root = dom.parseString(xml_string).documentElement
     except ExpatError:
         print(xml_string)
         raise
-    
+
     for request_node in requests_root.childNodes:
         indiv_columns_list = []
         indiv_values_list = []
-                
+
         if request_node.nodeType != xml.dom.Node.ELEMENT_NODE:
             continue
-    
+
         if request_node.tagName != 'request':
             raise Exception('Unexpected node: %s' % requests_root.toprettyxml())
-    
+
         for request_attr in request_node.childNodes:
             if request_attr.childNodes:
                 indiv_columns_list.append(request_attr.tagName)
                 indiv_values_list.append(request_attr.childNodes[0].data)
-        
+
         columns.append(indiv_columns_list)
         values.append(indiv_values_list)
     return (columns,values)
@@ -97,10 +97,19 @@ def insert_data(requests):
     columns,values = requests
 
     for i in range(len(values)):
-        together = dict(zip(columns[i], values[i]))
+
+        # Put the key-value pairs into a dictionary and then an arguments list.
+        request_dict = dict(zip(columns[i], values[i]))
+        together = request_dict
+
         r = Request(**together)
-        r.save()
-        print r.service_request_id
+
+        try:
+            r.save()
+            print "Successfully saved %s" % r.service_request_id
+        except Request.ValidationError:
+            raise CommandError('Request "%s" does not validate correctly' %
+                    r.service_request_id)
 
 # At runtime...
 class Command(BaseCommand):
@@ -119,7 +128,6 @@ class Command(BaseCommand):
         for _ in xrange(num_days):
             requests_stream = get_requests_from_SF(start, end)
             requests = parse_requests_doc(requests_stream) #returns tuple of columns and values
-            #insert_data_into_postgres(requests)
             insert_data(requests)
 
             start -= ONE_DAY
