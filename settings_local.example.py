32d4ef93027d811ad4f18821dad526c7026193bf
Chris Barna <chris@unbrain.net>
2011-06-20 10:29:52 -0700

    Install instructions.

---
+++ settings_local.example.py
@@ -0,0 +1,22 @@
+# Django local settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+# SECRET KEY
+SECRET_KEY = ''
+
+# CITY Variable
+CITY = {
+        'NAME': '',
+        'SHORTNAME': '',
+        'JURISDICTION': '',
+        'URL': r'',
+        }
