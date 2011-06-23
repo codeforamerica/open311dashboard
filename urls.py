bb71cb8fc508db194264d3363faa412e7d22f7f3
Chris Barna <chris@unbrain.net>
2011-06-23 10:30:56 -0700

    Changed URL patterns for API calls.

--- urls.py
+++ urls.py
@@ -6,16 +6,9 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'open311dashboard.dashboard.views.index'),
-    url(r'^tickets/$', 'open311dashboard.dashboard.views.ticket_days'),
-    url(r'^tickets/(?P<ticket_status>\w+)/$', 'open311dashboard.dashboard.views.ticket_days'),
-    url(r'^tickets/(?P<ticket_status>\w+)/(?P<start>.+)/(?P<end>.+)/$', 'open311dashboard.dashboard.views.ticket_days'),
-    # Examples:
-    # url(r'^$', 'open311dashboard.views.home', name='home'),
-    # url(r'^open311dashboard/', include('open311dashboard.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # API Calls
+    url(r'^api/tickets/$', 'open311dashboard.dashboard.views.ticket_days'),
+    url(r'^api/tickets/(?P<ticket_status>\w+)/$', 'open311dashboard.dashboard.views.ticket_days'),
+    url(r'^api/tickets/(?P<ticket_status>\w+)/(?P<start>\d{4}-\d{2}-\d{2})/(?P<end>\d{4}-\d{2}-\d{2})/$', 'open311dashboard.dashboard.views.ticket_days'),
 )
