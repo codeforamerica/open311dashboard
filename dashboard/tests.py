37737f1faddf437cefe9c8266b23ae9524a4bac0
Chris Barna <chris@unbrain.net>
2011-06-28 13:58:33 -0700

    Started writing tests for the API.

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,16 +1,17 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.utils import unittest
+from django.test.client import Client
 
+import json
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ApiTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_tickets(self):
+        response = self.client.get('/api/tickets/')
+
+        self.assertEqual(response.status_code, 200)
+
+        tickets = json.loads(response.content)
+        self.assertEqual(len(tickets), 30)
