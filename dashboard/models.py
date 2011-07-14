696d0619d4a3b7c583b65041763a1034042a39ae
Chris Barna <chris@unbrain.net>
2011-07-14 14:54:19 -0700

    Enabled PostGIS

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.contrib.gis.db import models
 from open311dashboard.settings import CITY
 
 class Request(models.Model):
--- settings.py
+++ settings.py
@@ -122,11 +122,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.gis',
     'open311dashboard.dashboard',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
