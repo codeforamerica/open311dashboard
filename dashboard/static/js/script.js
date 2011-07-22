15b9d6d66ac8c2c01114cce4621df44fbb4bc2b9
Chris Barna <chris@unbrain.net>
2011-07-21 20:51:28 -0700

    NOW with no warnings.

--- dashboard/static/js/script.js
+++ dashboard/static/js/script.js
@@ -10,9 +10,7 @@ $(function () {
             str = '0' + str;
         }
         return str;
-    };
-
-    var renderBarchart = function (fromDate, toDate) {
+    }, renderBarchart = function (fromDate, toDate) {
         $.ajax({
             url: '/api/tickets/both/' + fromDate.getFullYear() + '-' + pad(fromDate.getMonth() + 1, 2) + '-' + pad(fromDate.getDate(), 2) + '/' + toDate.getFullYear() + '-' + pad(toDate.getMonth() + 1, 2) + '-' + pad(toDate.getDate(), 2) + '/',
             dataType: 'json',
@@ -30,6 +28,7 @@ $(function () {
             renderBarchart($('#from').datepicker('getDate'), $('#to').datepicker('getDate'));
         }
     });
+
     $('#to').datepicker('setDate', new Date());
     $('#from').datepicker('setDate', -28);
 
