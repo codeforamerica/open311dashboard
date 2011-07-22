125e812baab478aa0d126710654b84cb04851b0d
Chris Barna <chris@unbrain.net>
2011-07-21 21:34:46 -0700

    JS shit is so weird.

--- dashboard/static/js/script.js
+++ dashboard/static/js/script.js
@@ -1,38 +1,37 @@
 /* Author: Chris Barna */
-/*global $, barchart */
-/*jslint browser: true, sloppy: false, maxerr: 50, indent: 4 */
+/* global barchart */
 $(function () {
-    'use strict';
+  'use strict';
 
-    var pad = function (number, length) {
-        var str = number.toString();
-        while (str.length < length) {
-            str = '0' + str;
-        }
-        return str;
-    }, renderBarchart = function (fromDate, toDate) {
-        $.ajax({
-            url: '/api/tickets/both/' + fromDate.getFullYear() + '-' + pad(fromDate.getMonth() + 1, 2) + '-' + pad(fromDate.getDate(), 2) + '/' + toDate.getFullYear() + '-' + pad(toDate.getMonth() + 1, 2) + '-' + pad(toDate.getDate(), 2) + '/',
-            dataType: 'json',
-            success: function (data) {
-                barchart.render(fromDate.getTime(), toDate.getTime(), data);
-            }
-        });
-    };
-
-
-    $("#from, #to").datepicker({
-        changeMonth: true,
-        onSelect: function () {
-            $("#chart").html('');
-            renderBarchart($('#from').datepicker('getDate'), $('#to').datepicker('getDate'));
-        }
+  var pad = function (number, length) {
+    var str = number.toString();
+    while (str.length < length) {
+      str = '0' + str;
+    }
+    return str;
+  }, renderBarchart = function (fromDate, toDate) {
+    $.ajax({
+      url: '/api/tickets/both/' + fromDate.getFullYear() + '-' + pad(fromDate.getMonth() + 1, 2) + '-' + pad(fromDate.getDate(), 2) + '/' + toDate.getFullYear() + '-' + pad(toDate.getMonth() + 1, 2) + '-' + pad(toDate.getDate(), 2) + '/',
+      dataType: 'json',
+      success: function (data) {
+        barchart.render(fromDate.getTime(), toDate.getTime(), data);
+      }
     });
+  };
 
-    $('#to').datepicker('setDate', new Date());
-    $('#from').datepicker('setDate', -28);
 
-    renderBarchart($("#from").datepicker('getDate'), $("#to").datepicker('getDate'));
+  $("#from, #to").datepicker({
+    changeMonth: true,
+    onSelect: function () {
+      $("#chart").html('');
+      renderBarchart($('#from').datepicker('getDate'), $('#to').datepicker('getDate'));
+    }
+  });
+
+  $('#to').datepicker('setDate', new Date());
+  $('#from').datepicker('setDate', -28);
+
+  renderBarchart($("#from").datepicker('getDate'), $("#to").datepicker('getDate'));
 
 });
 
