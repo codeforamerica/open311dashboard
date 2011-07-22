5f5f1e076bc0b83b712d02937920d674e696cda5
Chris Barna <chris@unbrain.net>
2011-07-21 17:24:24 -0700

    JSLint is a helluva thing.

--- dashboard/static/js/script.js
+++ dashboard/static/js/script.js
@@ -1,27 +1,27 @@
 /* Author: Chris Barna */
-var renderBarchart = function (fromDate, toDate) {
-    $.ajax({
-        url: '/api/tickets/both/'+fromDate.getFullYear()+'-'+pad(fromDate.getMonth() + 1, 2)+'-'+pad(fromDate.getDate(), 2)+'/'+toDate.getFullYear()+'-'+pad(toDate.getMonth()+1, 2)+'-'+pad(toDate.getDate(),2)+'/',
-        dataType: 'json',
-        success: function(data) {
-            barchart.render(fromDate.getTime(),toDate.getTime(),data);
-        }
-    });
-};
-
-var pad = function(number, length) {
-    var str = '' + number;
+var pad = function (number, length) {
+    var str = number.toString();
     while (str.length < length) {
         str = '0' + str;
     }
     return str;
 };
 
-$(function() {
+var renderBarchart = function (fromDate, toDate) {
+    $.ajax({
+        url: '/api/tickets/both/' + fromDate.getFullYear() + '-' + pad(fromDate.getMonth() + 1, 2) + '-' + pad(fromDate.getDate(), 2) + '/' + toDate.getFullYear() + '-' + pad(toDate.getMonth() + 1, 2) + '-' + pad(toDate.getDate(), 2) + '/',
+        dataType: 'json',
+        success: function (data) {
+            barchart.render(fromDate.getTime(), toDate.getTime(), data);
+        }
+    });
+};
+
+$(function () {
 
     $("#from, #to").datepicker({
         changeMonth: true,
-        onSelect: function(selectedDate) {
+        onSelect: function () {
             $("#chart").html('');
             renderBarchart($('#from').datepicker('getDate'), $('#to').datepicker('getDate'));
         }
@@ -29,7 +29,7 @@ $(function() {
     $('#to').datepicker('setDate', new Date());
     $('#from').datepicker('setDate', -28);
 
-    renderBarchart($("#from").datepicker('getDate'),$("#to").datepicker('getDate'));
+    renderBarchart($("#from").datepicker('getDate'), $("#to").datepicker('getDate'));
 
 });
 
