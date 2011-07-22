9c318a01c1ba547e32f1238c4e4db127b79175e3
Chris Barna <chris@unbrain.net>
2011-07-21 17:13:02 -0700

    Moved script into separate file. Other semantic changes.

--- dashboard/static/js/script.js
+++ dashboard/static/js/script.js
@@ -1,3 +1,35 @@
-/* Author: 
+/* Author: Chris Barna */
+var renderBarchart = function (fromDate, toDate) {
+    $.ajax({
+        url: '/api/tickets/both/'+fromDate.getFullYear()+'-'+pad(fromDate.getMonth() + 1, 2)+'-'+pad(fromDate.getDate(), 2)+'/'+toDate.getFullYear()+'-'+pad(toDate.getMonth()+1, 2)+'-'+pad(toDate.getDate(),2)+'/',
+        dataType: 'json',
+        success: function(data) {
+            barchart.render(fromDate.getTime(),toDate.getTime(),data);
+        }
+    });
+};
 
-*/
+var pad = function(number, length) {
+    var str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+    return str;
+};
+
+$(function() {
+
+    $("#from, #to").datepicker({
+        changeMonth: true,
+        onSelect: function(selectedDate) {
+            $("#chart").html('');
+            renderBarchart($('#from').datepicker('getDate'), $('#to').datepicker('getDate'));
+        }
+    });
+    $('#to').datepicker('setDate', new Date());
+    $('#from').datepicker('setDate', -28);
+
+    renderBarchart($("#from").datepicker('getDate'),$("#to").datepicker('getDate'));
+
+});
+
