5db620df2d022e8ce8440a23f3531d55cb682ff6
Chris Barna <chris@unbrain.net>
2011-08-15 12:02:32 -0700

    Updating urls.py.

--- urls.py
+++ urls.py
@@ -20,41 +20,8 @@ urlpatterns = patterns('',
     url(r'^search/$',
         'open311dashboard.dashboard.views.street_search'),
 
-    # Login
-    url(r'^login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'login.html'}),
-
     # API Calls
     url(r'^api/home/(?P<geography>\d+).json$',
         'open311dashboard.dashboard.views.index', {'is_json':True}),
 
-    url(r'^api/tickets/$', 'open311dashboard.dashboard.views.ticket_days'),
-    url(r'^api/tickets/(?P<ticket_status>open|closed|both)/$',
-        'open311dashboard.dashboard.views.ticket_days'),
-    url(r'^api/tickets/(?P<ticket_status>open|closed|both)/(?P<num_days>\d+)/$',
-        'open311dashboard.dashboard.views.ticket_days'),
-    url(r'^api/tickets/(?P<ticket_status>open|closed|both)/(?P<end>\d{4}-\d{2}-\d{2})/$',
-        'open311dashboard.dashboard.views.ticket_days'),
-    url(r'^api/tickets/(?P<ticket_status>open|closed|both)/(?P<end>\d{4}-\d{2}-\d{2})/(?P<num_days>\d+)/',
-        'open311dashboard.dashboard.views.ticket_days'),
-    url(r'^api/tickets/(?P<ticket_status>open|closed|both)/(?P<start>\d{4}-\d{2}-\d{2})/(?P<end>\d{4}-\d{2}-\d{2})/$',
-        'open311dashboard.dashboard.views.ticket_days'),
-
-    # More in depth aggregate data.
-    url(r'^api/more_info/(?P<begin>\d{4}-\d{2}-\d{2})/$',
-        'open311dashboard.dashboard.views.ticket_day'),
-    url(r'^api/more_info/(?P<begin>\d{4}-\d{2}-\d{2})/(?P<end>\d{4}-\d{2}-\d{2})/$',
-        'open311dashboard.dashboard.views.ticket_day'),
-
-    # All tickets within a range of dates.
-    url(r'^api/list/(?P<begin>\d{4}-\d{2}-\d{2})/$',
-        'open311dashboard.dashboard.views.list_requests'),
-    url(r'^api/list/(?P<begin>\d{4}-\d{2}-\d{2})/(?P<end>\d{4}-\d{2}-\d{2})/$',
-        'open311dashboard.dashboard.views.list_requests'),
-
-    # Handle admin stuff.
-    url(r'^admin/$', 'open311dashboard.dashboard.views.admin'),
-    url(r'^admin/(?P<shortname>\w+)/$',
-        'open311dashboard.dashboard.views.city_admin'),
-    url(r'^admin/new/$', 'open311dashboard.dashboard.views.city_add'),
 )
