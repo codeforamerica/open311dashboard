595edd6b6246d77f1b56d59dd94bb36a985ba52c
Zach Williams <hey@zachwill.com>
2011-08-22 20:40:09 -0700

    Adjusted the search page

--- dashboard/static/js/autocomplete.js
+++ dashboard/static/js/autocomplete.js
@@ -1,18 +1,42 @@
-$(function() {
+(function(window, $) {
+
+  window._results = {};
+
   var search = $('input[type="text"]'),
       geocoder = new google.maps.Geocoder();
 
   search.autocomplete({
-    source: function(request, response){
+    select: function(event, ui) {
+        var url = '/search/?',
+            value = ui.item.value,
+            parameters = window._results[value];
+        parameters['q'] = value;
+        console.log(parameters);
+        window.location = url + $.param(parameters);
+    },
+    source: function(request, response) {
       // Grab the source objects from Google Maps.
-      geocoder.geocode({'address': request.term}, function(results, status){
-        response($.map(results, function(item){
-          return {
-            label: item.formatted_address,
-            value: item.formatted_address
-          }
-        }));
-      });
+      var southwest = new google.maps.LatLng(37.68, -122.57),
+          northeast = new google.maps.LatLng(37.84, -122.35),
+          bounds = new google.maps.LatLngBounds(southwest, northeast);
+
+      geocoder.geocode({
+          'address': request.term,
+          'bounds': bounds
+      }, function(results, status){
+           response($.map(results, function(item){
+               var location = item.geometry.location,
+                   coordinates = {
+                    'lat': location.lat(),
+                    'lng': location.lng()
+                   };
+             window._results[item.formatted_address] = coordinates;
+             return {
+               label: item.formatted_address,
+               value: item.formatted_address
+             }
+           }));
+        });
     }
   });
-});
+})(window, jQuery);
