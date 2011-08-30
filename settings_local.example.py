2afc99f4214304d6bf36c136ebaf5a36c4df3f23
Justin Palmer <justin@labratrevenge.com>
2011-08-30 13:41:07 -0700

    Add missing settings to example file

--- settings_local.example.py
+++ settings_local.example.py
@@ -12,3 +12,14 @@ DATABASES = {
 
 # SECRET KEY
 SECRET_KEY = ''
+
+# Enagle Geographic data
+ENABLE_GEO = True
+
+# Open311 City
+# See http://wiki.open311.org/GeoReport_v2/Servers
+CITY = {
+  'URL': 'https://open311.sfgov.org/dev/Open311/v2/requests.xml',
+  'PAGINATE': True,
+  'JURISDICTION': 'sfgov.org'
+}
