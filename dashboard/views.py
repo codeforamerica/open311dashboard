05ef604bb896b4be6a859ef552ccda4bb2ca587f
Chris Barna <chris@unbrain.net>
2011-09-05 20:11:08 -0700

    Clearing out views.py. neighborhood_list has mongo backend.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,66 +1,29 @@
 import datetime
-import qsstats
 import time
 import json
 import urllib
 import urllib2
 
 from django.template import Context
-from django.shortcuts import render, redirect
-from django.db.models import Count
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render # , redirect
+# from django.http import HttpResponse
 
-from django.contrib.gis.geos import Point
-from django.contrib.gis.measure import Distance as D
+from pymongo.connection import Connection
 
-from dashboard.models import Request, City, Geography, Street
+from settings import MONGODB
+
 from dashboard.decorators import ApiHandler
-from dashboard.utils import str_to_day, day_to_str, \
-    date_range, dt_handler, render_to_geojson, run_stats, calculate_delta, \
-    json_response_from
 
+# Set up the database connection as a global variable
+connection = Connection(MONGODB['host'])
+db = connection[MONGODB['db']]
 
 def index(request, geography=None, is_json=False):
     """
     Homepage view. Can also return json for the city or neighborhoods.
     """
-    if geography is None:
-        requests = Request.objects.all()
-    else:
-        neighborhood = Geography.objects.get(pk=geography)
-        requests = Request.objects.filter(geo_point__contained=neighborhood.geo)
 
-    total_open = requests.filter(status="Open").count()
-    most_recent = requests.latest('requested_datetime')
-    minus_7 = most_recent.requested_datetime-datetime.timedelta(days=7)
-    minus_14 = most_recent.requested_datetime-datetime.timedelta(days=14)
-
-    this_week = requests.filter(requested_datetime__range= \
-            (minus_7, most_recent.requested_datetime))
-    last_week = requests.filter(requested_datetime__range= \
-            (minus_14, minus_7))
-
-    this_week_stats = run_stats(this_week, request_types=False,
-            open_requests=False)
-    last_week_stats = run_stats(last_week, request_types=False,
-            open_requests=False)
-
-    # Calculate deltas
-    delta = {}
-    delta['count'] = calculate_delta(this_week_stats['request_count'],
-            last_week_stats['request_count'])
-    delta['closed_count'] = calculate_delta( \
-            this_week_stats['closed_request_count'],
-            last_week_stats['closed_request_count'])
-    delta['opened_count'] = calculate_delta( \
-            this_week_stats['open_request_count'],
-            last_week_stats['open_request_count'])
-    delta['time'] = calculate_delta(this_week_stats['average_response'],
-            last_week_stats['average_response'])
-
-    # Put everything in a dict so we can do what we want with it.
-    c_dict = {
+    """c_dict = {
         'open_tickets': total_open,
         'this_week_stats': this_week_stats,
         'last_week_stats': last_week_stats,
@@ -75,7 +38,7 @@ def index(request, geography=None, is_json=False):
         return render(request, 'index.html', c)
     else:
         data = json.dumps(c_dict, True)
-        return HttpResponse(data, content_type='application/json')
+        return HttpResponse(data, content_type='application/json')"""
 
 
 # Neighborhood specific pages.
@@ -83,7 +46,8 @@ def neighborhood_list(request):
     """
     List the neighborhoods.
     """
-    neighborhoods = Geography.objects.all()
+    # neighborhoods = Geography.objects.all()
+    neighborhoods = db.polygons.find({ 'properties.type' : 'neighborhood' })
 
     c = Context({
         'neighborhoods': neighborhoods
@@ -98,21 +62,7 @@ def neighborhood_detail(request, neighborhood_id):
     Show detail for a specific neighborhood. Uses templates/geo_detail.html
 
     """
-    neighborhood = Geography.objects.get(pk=neighborhood_id)
-    nearby = Geography.objects.all().distance(neighborhood.geo) \
-            .exclude(name=neighborhood.name).order_by('distance')[:5]
-
-    # Get the requests inside the neighborhood, run the stats
-    requests = Request.objects.filter(geo_point__contained=neighborhood.geo)
-    stats = run_stats(requests)
-
-    title = neighborhood.name
-
-    neighborhood.geo.transform(4326)
-    simple_shape = neighborhood.geo.simplify(.0003,
-            preserve_topology=True)
-
-    c = Context({
+    """c = Context({
         'title': title,
         'geometry': simple_shape.geojson,
         'centroid': [simple_shape.centroid[0], simple_shape.centroid[1]],
@@ -123,7 +73,7 @@ def neighborhood_detail(request, neighborhood_id):
         'id': neighborhood_id
         })
 
-    return render(request, 'geo_detail.html', c)
+    return render(request, 'geo_detail.html', c) """
 
 
 def neighborhood_detail_json(request, neighborhood_id):
@@ -134,10 +84,6 @@ def neighborhood_detail_json(request, neighborhood_id):
     TODO: Speed it up.
 
     """
-    neighborhood = Geography.objects.get(pk=neighborhood_id)
-    requests = Request.objects.filter(geo_point__contained=neighborhood.geo)
-    return json_response_from(requests)
-
 
 # Street specific pages.
 def street_list(request):
@@ -146,15 +92,11 @@ def street_list(request):
     List the top 10 streets by open service requests.
 
     """
-    streets = Street.objects.filter(request__status="Open") \
-            .annotate(count=Count('request__service_request_id')) \
-            .order_by('-count')[:10]
-
-    c = Context({
+    """c = Context({
         'top_streets': streets
     })
 
-    return render(request, 'street_list.html', c)
+    return render(request, 'street_list.html', c) """
 
 
 def street_view(request, street_id):
@@ -162,26 +104,7 @@ def street_view(request, street_id):
     View details for a specific street. Renders geo_detail.html like
     neighborhood_detail does.
     """
-    street = Street.objects.get(pk=street_id)
-    nearby = Street.objects.all().distance(street.line) \
-            .exclude(street_name=street.street_name).order_by('distance')[:5]
-    neighborhood = Geography.objects.all() \
-            .distance(street.line).order_by('distance')[:1]
-
-    # Max/min addresses
-    addresses = [street.left_low_address, street.left_high_address,
-                 street.right_low_address, street.right_high_address]
-    addresses.sort()
-
-    title = "%s %i - %i" % (street.street_name, addresses[0], addresses[3])
-
-    # Requests
-    requests = Request.objects.filter(street=street_id)
-    stats = run_stats(requests)
-
-    street.line.transform(4326)
-
-    c = Context({
+    """c = Context({
         'title': title,
         'geometry': street.line.geojson,
         'centroid': [street.line.centroid[0], street.line.centroid[1]],
@@ -193,7 +116,7 @@ def street_view(request, street_id):
         'id': street_id
         })
 
-    return render(request, 'geo_detail.html', c)
+    return render(request, 'geo_detail.html', c) """
 
 
 def street_view_json(request, street_id):
@@ -202,9 +125,6 @@ def street_view_json(request, street_id):
     Download the JSON for the requests that built the page.
 
     """
-    requests = Request.objects.filter(street=street_id)
-    return json_response_from(requests)
-
 
 # Search for an address!
 def street_search(request):
@@ -239,10 +159,7 @@ def street_search(request):
         else:
             lat, lon = None, None
 
-    if lat and lon:
-        point = Point(float(lon), float(lat))
-        point.srid = 4326
-        point.transform(900913)
+    """ if lat and lon:
         nearest_street = Street.objects \
                                .filter(line__dwithin=(point, D(m=100))) \
                                .distance(point).order_by('distance')[:1]
@@ -251,7 +168,7 @@ def street_search(request):
         except IndexError:
             pass
     c = Context({'error': True})
-    return render(request, 'search.html', c)
+    return render(request, 'search.html', c) """
 
 
 def map(request):
@@ -263,116 +180,12 @@ def map(request):
     """
     return render(request, 'map.html')
 
-# Admin Pages
-@login_required
-def admin(request):
-    """
-
-    Admin home page. Just list the cities.
-
-    """
-    cities = City.objects.all()
-    c = Context({'cities': cities})
-    return render(request, 'admin/index.html', c)
-
-@login_required
-def city_admin(request, shortname=None):
-    """
-
-    Administer a specific city (and associated data)
-
-    """
-    city = City.objects.get(short_name=shortname)
-    geographies = Geography.objects.filter(city=city.id).count()
-    streets = Street.objects.filter(city=city.id).count()
-    requests = Request.objects.filter(city=city.id).count()
-
-    c = Context({
-        'city': city,
-        'geographies': geographies,
-        'streets': streets,
-        'requests': requests
-        })
-
-    return render(request, 'admin/city_view.html', c)
-
-@login_required
-def city_add(request):
-    """
-
-    Add a new city.
-
-    """
-    return render(request, 'admin/city_add.html')
-
 # API Views
 @ApiHandler
 def ticket_days(request, ticket_status="open", start=None, end=None,
                 num_days=None):
     '''Returns JSON with the number of opened/closed tickets in a specified
     date range'''
-
-    # If no start or end variables are passed, do the past 30 days. If one is
-    # passed, check if num_days and do the past num_days. If num_days isn't
-    # passed, just do one day. Else, do the range.
-    if start is None and end is None:
-        num_days = int(num_days) if num_days is not None else 29
-
-        end = datetime.date.today()
-        start = end - datetime.timedelta(days=num_days)
-    elif end is not None and num_days is not None:
-        num_days = int(num_days) - 1
-        end = str_to_day(end)
-        start = end - datetime.timedelta(days=num_days)
-    elif end is not None and start is None:
-        end = str_to_day(end)
-        start = end
-    else:
-        start = str_to_day(start)
-        end = str_to_day(end)
-
-    if ticket_status == "open":
-        request = Request.objects.filter(status="Open") \
-            .filter(requested_datetime__range=date_range(day_to_str(start),
-                                                         day_to_str(end)))
-        stats = qsstats.QuerySetStats(request, 'requested_datetime')
-    elif ticket_status == "closed":
-        request = Request.objects.filter(status="Closed")
-        stats = qsstats.QuerySetStats(request, 'updated_datetime') \
-            .filter(requested_datetime__range=date_range(day_to_str(start),
-                                                         day_to_str(end)))
-    elif ticket_status == "both":
-        request_opened = Request.objects.filter(status="Open") \
-            .filter(requested_datetime__range=date_range(day_to_str(start),
-                                                         day_to_str(end)))
-        stats_opened = qsstats.QuerySetStats(request_opened,
-                                             'requested_datetime')
-
-        request_closed = Request.objects.filter(status="Closed") \
-            .filter(requested_datetime__range=date_range(day_to_str(start),
-                                                         day_to_str(end)))
-        stats_closed = qsstats.QuerySetStats(request_closed,
-                                             'updated_datetime')
-
-    data = []
-
-    try:
-        raw_data = stats.time_series(start, end)
-
-        for row in raw_data:
-            temp_data = {'date': int(time.mktime(row[0].timetuple())), 'count': row[1]}
-            data.append(temp_data)
-    except:
-        opened_data = stats_opened.time_series(start, end)
-        closed_data = stats_closed.time_series(start, end)
-        for i in range(len(opened_data)):
-            temp_data = {
-                'date': int(time.mktime(opened_data[i][0].timetuple())),
-                'open_count': opened_data[i][1],
-                'closed_count': closed_data[i][1],
-            }
-            data.append(temp_data)
-    return data
 
 @ApiHandler
 def ticket_day(request, begin=day_to_str(datetime.date.today()), end=None):
@@ -381,19 +194,6 @@ def ticket_day(request, begin=day_to_str(datetime.date.today()), end=None):
     Get service_name stats for a range of dates.
 
     """
-    if end == None:
-        key = begin
-    else:
-        key = "%s - %s" % (begin, end)
-
-    # Request and group by service_name.
-    requests = Request.objects \
-            .filter(requested_datetime__range=date_range(begin, end)) \
-            .values('service_name').annotate(count=Count('service_name')) \
-            .order_by('-count')
-
-    data = {key: [item for item in requests]}
-    return data
 
 # List requests in a given date range
 @ApiHandler
@@ -403,8 +203,3 @@ def list_requests(request, begin=day_to_str(datetime.date.today()), end=None):
     List requests opened in a given date range
 
     """
-    requests = Request.objects \
-        .filter(requested_datetime__range=date_range(begin,end))
-
-    data = [item for item in requests.values()]
-    return data
