7b8d881f3f5ccbeb009b918c83b4eb2612d9b081
joanne cheng <chengjoanne2@gmail.com>
2011-09-13 13:55:22 -0600

    changes to open311stats.py

--- mongoutils/mapreduce/open311stats.py
+++ mongoutils/mapreduce/open311stats.py
@@ -5,16 +5,30 @@ connection = Connection('localhost')
 db = connection['open311']
 
 def mreduce(map_file, reduce_file):
-  map = Code(open(map_file, 'r').read())
-  reduce = Code(open(reduce_file, 'r').read())
+  map = Code(map_file)
+  reduce = Code(reduce_file)
   return db.requests.inline_map_reduce(map, reduce)
 
 def day_counts():
-  return mreduce('day_map.js', 'day_reduce.js')
+  return mreduce(open('day_map.js', 'r').read(), open('day_reduce.js', 'r').read())
 
 def month_counts():
-  return mreduce('month_map.js', 'day_reduce.js')
+  return mreduce(open('month_map.js', 'r').read(), open('day_reduce.js','r').read())
 
 def year_counts():
-  return mreduce('year_map.js', 'day_reduce.js')
+  return mreduce(open('year_map.js', 'r').read(), open('day_reduce.js', 'r').read())
 
+def avg_response_time(start_time=null, end_time=null):
+  map_str = """
+      function(){
+        var diff = new Date();
+        if (this.updated_datetime != null && this.requested_datetime != null){
+  """
+  if start_time == null and end_time == null:
+    map_str += """
+          var u_date = new Date(this.updated_datetime);
+          var r_date = new Date(this.requested_datetime);
+          diff.setTime(r_date - u_date);
+        }
+      }
+    """
