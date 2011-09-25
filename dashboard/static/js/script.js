b0077d60e8b33343415f65abc33ea9560f160396
Chris Barna <chris@unbrain.net>
2011-09-25 12:39:22 -0700

    Open311Dashboard JS namespace.

--- dashboard/static/js/script.js
+++ dashboard/static/js/script.js
@@ -1,37 +1,20 @@
-/* Author: Chris Barna */
-/* global barchart */
-$(function () {
-  'use strict';
+/* Authors: @ctbarna, @evansml */
+/* Helper methods for the open311dashboard */
 
-  var pad = function (number, length) {
-    var str = number.toString();
-    while (str.length < length) {
-      str = '0' + str;
-    }
-    return str;
-  }, renderBarchart = function (fromDate, toDate) {
-    $.ajax({
-      url: '/api/tickets/both/' + fromDate.getFullYear() + '-' + pad(fromDate.getMonth() + 1, 2) + '-' + pad(fromDate.getDate(), 2) + '/' + toDate.getFullYear() + '-' + pad(toDate.getMonth() + 1, 2) + '-' + pad(toDate.getDate(), 2) + '/',
-      dataType: 'json',
-      success: function (data) {
-        barchart.render(fromDate.getTime(), toDate.getTime(), data);
-      }
-    });
-  };
+// Define a global namespace.
+Open311Dashboard = {};
 
+// Map namespace.
+Open311Dashboard.map = {};
 
-  $("#from, #to").datepicker({
-    changeMonth: true,
-    onSelect: function () {
-      $("#chart").html('');
-      renderBarchart($('#from').datepicker('getDate'), $('#to').datepicker('getDate'));
-    }
-  });
+// Pad a bounding box for a map.
+Open311Dashboard.map.padBox = function (box, padding) {
+  box[0].lat -= padding;
+  box[0].lon -= padding;
+  box[1].lat += padding;
+  box[1].lon += padding;
+  return box;
+};
 
-  $('#to').datepicker('setDate', new Date());
-  $('#from').datepicker('setDate', -28);
-
-  renderBarchart($("#from").datepicker('getDate'), $("#to").datepicker('getDate'));
-
-});
-
+// API Namespace.
+Open311Dashboard.API = {};
