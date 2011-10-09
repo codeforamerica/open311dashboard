f27706bea98fb97061a6f87ac3a8507eaa0e21a2
Chris Barna <chris@unbrain.net>
2011-10-09 14:03:52 -0700

    Google StreetView on dblclick of open requests.

--- dashboard/static/js/o3d.api.js
+++ dashboard/static/js/o3d.api.js
@@ -38,6 +38,12 @@ o3d.API.buildPage = function loadStats() {
       $("#average-response-value").html(o3d.time.ms2human(data.avg_time));
   });
 
+  // Trigger the resize event when we show the modal.
+  $("#my-modal").modal({backdrop: true});
+  $('#my-modal').bind("shown", function() {
+    google.maps.event.trigger(myPano, 'resize');
+  });
+
   // Load the map data.
   $.get('/api/requests/?'+$.param(o3d.API.query)+'&status=Open&page_size=10&sort=-requested_datetime',
     function(data) {
@@ -46,7 +52,8 @@ o3d.API.buildPage = function loadStats() {
 
       for (var i = 0; i < data.length; i += 1) {
         // Generate the rows.
-        var tmp_data = "<tr id=\""+data[i]['service_request_id']+ "-row\">";
+        var tmp_data = "<tr id=\""+data[i]['service_request_id']+ "-row\" ";
+        tmp_data += "data-lat=\""+data[i]['coordinates'][1]+"\" data-long=\""+data[i]['coordinates'][0]+"\">";
         tmp_data += "<td>" + data[i]['service_request_id']+ "</td>";
         tmp_data += "<td>" + data[i]['service_name']+ "</td>";
         tmp_data += "<td>" + data[i]['status'] + "</td>";
@@ -65,6 +72,17 @@ o3d.API.buildPage = function loadStats() {
           }, "properties" : {
             "id": data[i]['service_request_id']
           }
+        });
+
+        // Street view.
+        $("#"+data[i]['service_request_id']+"-row").dblclick(function() {
+          $("#street-view-place").html('');
+          var point = new google.maps.LatLng($(this).data('lat'), $(this).data('long'));
+          var options = { position: point };
+          myPano = new google.maps.StreetViewPanorama(document.getElementById('street-view-place'), options);
+          $("#my-modal").modal('show');
+          myPano.setVisible(true);
+          google.maps.event.trigger(myPano, 'resize');
         });
 
         // Show or hide points.
