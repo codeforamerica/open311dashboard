78b1587d21414c9879212105ff4c09e0cc3cd03a
Chris Barna <chris@unbrain.net>
2011-10-10 22:22:06 -0700

    Fixing update_db

--- mongoutils/migrations/requests.py
+++ mongoutils/migrations/requests.py
@@ -15,7 +15,7 @@ def add_indexes():
     db.requests.ensure_index([('coordinates', '2d')])
 
 def drop_indexes():
-    db.requests.drop_index('coordinates_2d')
+    db.requests.drop_indexes()
 
 def find_nearest_street(coordinate_list, max_distance):
     """
